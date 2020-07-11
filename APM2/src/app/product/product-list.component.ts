0d3ae5bb202d868c0d11db98496d37597e868100
Bharath Kumar <1.bharath.k@gmail.com>
2020-07-11 15:54:47 +0530

    Adding Services and HTTP by Observable

--- APM2/src/app/product/product-list.component.ts
+++ APM2/src/app/product/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -9,9 +10,6 @@ import { IProduct } from './product';
 })
 export class ProductListComponent implements OnInit {
 
-  ngOnInit(): void {
-      console.log("Sitting in on Init method");
-    }
 
     pageTitle: string = 'Product List!';
     imageWidth: number = 50;
@@ -37,47 +35,25 @@ export class ProductListComponent implements OnInit {
             product.productName.toLowerCase().indexOf(filterBy) !== -1);
     }
 
-    constructor() {
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+    products: IProduct[] = [];
+    errorMessage: string;
+    constructor(private productService: ProductService) {
     }
+
+    ngOnInit(): void {
+      this.productService.getProducts().subscribe({
+        next: products => {this.products = products; this.filteredProducts = this.products;},
+        error: err => this.errorMessage = err,
+        //complete: complete fn
+      });      
+    }
+
+
 
     onRatingClicked(message: string): void {
       this.pageTitle='Product List: ' + message;
     }
 
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2019",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2019",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2019",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        }
-    ];
 
     toggleImage(): void {
       this.showImage = !this.showImage;
