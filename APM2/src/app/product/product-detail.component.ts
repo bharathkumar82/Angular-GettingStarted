520a68bf3c52acf795befd4d322c618b96c286b1
Bharath Kumar <1.bharath.k@gmail.com>
2020-07-12 19:10:37 +0530

    Dynamic Routing

--- APM2/src/app/product/product-detail.component.ts
+++ APM2/src/app/product/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -11,7 +11,12 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  onBack(): void {
+    this.router.navigate(['/products']); //This is code equivalent of routerLink tag
+  }
+
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
