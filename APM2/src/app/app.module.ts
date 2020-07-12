47a0b739cf062bd7b1045eb0f03f3736f67f0e5e
Bharath Kumar <1.bharath.k@gmail.com>
2020-07-12 11:54:44 +0530

    Simple Navigation

--- APM2/src/app/app.module.ts
+++ APM2/src/app/app.module.ts
@@ -6,10 +6,21 @@ import { ProductListComponent } from './product/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent, WelcomeComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, 
+            RouterModule.forRoot([
+              { path: 'products', component: ProductListComponent }, 
+              { path: 'product/:id', component: ProductDetailComponent },
+              { path: 'welcome', component: WelcomeComponent },
+              { path: '', redirectTo: 'welcome', pathMatch:'full' },
+              { path: '**', redirectTo: 'welcome', pathMatch:'full' }
+            ])
+           ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
